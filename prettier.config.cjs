ddaf2b7426269f89f7cd62850da0c9022cfa7d04
Giovanni Sena <giovanni.lima786@gmail.com>
2025-07-02 22:19:23 -0300

    configurar lint automatico para identacao e sort das classes tailwind

--- prettier.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import("prettier").Config} */
-module.exports = {
-  plugins: ['prettier-plugin-tailwindcss'],
-  printWidth: 100,
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'es5',
-}
