db1a2bc83c6e3eb1d0ed9893fe882a1b36729d38
Giovanni Sena <giovanni.lima786@gmail.com>
2025-07-02 21:22:36 -0300

    criar iteracao e constante para os campos de cadastro

---
+++ src/constants/inputsRegister.ts
@@ -0,0 +1,54 @@
+const inputsRegister = [
+  {
+    name: 'name',
+    label: 'Nome completo',
+    type: 'text',
+    placeholder: 'Digite o nome completo',    
+  },
+  {
+    name: 'cpf',
+    label: 'CPF',
+    type: 'text',
+    placeholder: 'Digite o CPF',    
+  },
+  {
+    name: 'phone',
+    label: 'Telefone',
+    type: 'text',
+    placeholder: 'Digite o telefone',    
+  },
+  {
+    name: 'dateOfBirth',
+    label: 'Data de nascimento',
+    placeholder: 'Digite a data de nascimento',
+  },
+  {
+    name: 'maritalState',
+    label: 'Estado Civil',
+    placeholder: 'Digite o estado civil',
+  },
+  {
+    name: 'gender',
+    label: 'Genero',
+    placeholder: 'Selecione o gênero',
+  },
+  {
+    name: 'university',
+    label: 'Universitário',
+    placeholder: '',
+  },
+   {
+    name: 'neighborhood',
+    label: 'Bairro',
+    placeholder: 'Digite o nome do bairro',
+  },
+  {
+    name: 'cellule',
+    label: 'Célula',
+    placeholder: 'Selecione a célula',
+  },
+]
+
+
+
+export { inputsRegister }
---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,24 @@
+import InputText from "@/components/Input/Input"
+import { inputsRegister } from "@/constants/inputsRegister"
+
+function Register() {
+    return (
+        <article className='mt-2 flex w-full flex-col justify-start gap-3.5 p-8'>
+            <div >
+                <h2 className="text-2xl">Cadastro</h2>
+            </div>
+            <div className="w-full flex-col">
+            {inputsRegister.map((input) => {   
+                return (
+                <div className="mt-4" key={input.label}>
+                    <InputText label={input.label}/>
+                </div>
+                )       
+            })}
+                
+            </div>
+        </article>
+    )
+}
+
+export default Register
